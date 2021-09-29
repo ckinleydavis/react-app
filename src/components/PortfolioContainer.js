256c32e4c6878fe8a17cb01fde79e05a69bdf9ac
Kinley Davis <d8tascientist@gmail.com>
2021-09-29 19:52:44 -0400

    adding container

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Nav from './Nav';
+import App from '../App'; // Homepage
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+    // Set home page as default current page
+    const [ currentPage, setCurrentPage ] = useState('Home');
+
+    // Check which page we are on
+    const pageMonitor = () => {
+        if ( currentPage === 'Home' ) {
+            return <App />;
+        }
+        if ( currentPage === 'About' ) {
+            return <About />;
+        }
+        if ( currentPage === 'About' ) {
+            return <Portfolio />;
+        }
+        if ( currentPage === 'About' ) {
+            return <Contact />;
+        }
+        if ( currentPage === 'About' ) {
+            return <Resume />;
+        }
+    };
+
+    const pageChangeHandler = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Nav 
+                currentPage={currentPage} 
+                pageChangeHandler={pageChangeHandler} 
+            />
+            { pageMonitor() }
+        </div>
+    );
+}
